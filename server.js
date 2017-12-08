6692d6580a9290ce8120632ed112e447977fe684
Seyedamirhossein Hesamian <amir734jj@live.com>
2017-12-08 11:37:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ lineReader.on('line', function (line) {
        .replace("'", "")
        .replace(".", "")
        .replace(",", "")
-       .replace('"', "")                                        
+       .replace('"', "")
+       .replace("]", "")
+       .replace("[", "")
      ));
 });
 
